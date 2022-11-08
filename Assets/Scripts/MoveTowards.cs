153cf070253a980644c12765ce8a9ab2b97fe65a
EthanM0113 <102124492+EthanM0113@users.noreply.github.com>
2022-11-08 14:53:41 +0800

    Added Beta Testing Stuff

---
+++ Assets/Scripts/MoveTowards.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTowards : MonoBehaviour
+{
+    [SerializeField] private GameObject target;
+    [SerializeField] private string targetTag;
+    [SerializeField] private float speed;
+     
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag(targetTag);
+        speed = Random.Range(speed, speed + 4f); // randomize speed
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LookTowardsTarget();
+        MoveTowardsTarget();
+    }
+
+    public void LookTowardsTarget()
+    {
+        transform.right = target.transform.position - transform.position;
+    }
+
+    public void MoveTowardsTarget()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+    }
+}
