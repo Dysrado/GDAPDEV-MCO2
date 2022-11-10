d2d06f05574115135e8f327fbda213eb42e4dd82
EthanM0113 <102124492+EthanM0113@users.noreply.github.com>
2022-11-10 17:50:44 +0800

    Added Interval Projectile System for 1st Boss

--- Assets/Scripts/FirstBossManager.cs
+++ Assets/Scripts/FirstBossManager.cs
@@ -17,25 +17,64 @@ public enum ShieldType
 public class FirstBossManager : MonoBehaviour
 {
     public FirstBossState bossState;
+
     [SerializeField] private GameObject shieldSpawner;
     [SerializeField] private GameObject shieldPrefab;
+    [SerializeField] private GameObject projectileSpawner;
+    [SerializeField] private GameObject projectilePrefab;
+
     public GameObject shield;
+
+    public float projectileSpeed;
+    private float projectileTicks;
+    private float projectileInterval;
+    public float projectileMaxInterval;
+    public List<GameObject> projectiles;
+    public GameObject projectile;
+
+
+    [SerializeField] private GameObject playerCenter;
 
     // Start is called before the first frame update
     void Start()
     {
         bossState = FirstBossState.SHIELDED;
         SpawnShield();
+        SpawnProjectile();
+        projectileTicks = 0f;
+        projectileInterval = 3; // Default value
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        projectileTicks += Time.deltaTime;
+        if(projectileTicks > projectileInterval)
+        {
+            SpawnProjectile();
+            projectileTicks = 0f;
+            projectileInterval = Random.Range(3, projectileMaxInterval + 1);
+        }
+        UpdateProjectiles();
     }
 
     public void SpawnShield()
     {
         shield = GameObject.Instantiate(shieldPrefab, shieldSpawner.transform);
+    }
+
+    public void SpawnProjectile()
+    {
+        projectile = GameObject.Instantiate(projectilePrefab, projectileSpawner.transform);
+        projectiles.Add(projectile);
+
+    }
+
+    public void UpdateProjectiles()
+    {
+        for(int i = 0; i < projectiles.Count; i++)
+        {
+            projectiles[i].transform.position = Vector3.MoveTowards(projectiles[i].transform.position, playerCenter.transform.position, projectileSpeed * Time.deltaTime);
+        }
     }
 }
