a7d6c8ad59ba05de15480c17c500234db846e0fa
Dysrado <63017345+Dysrado@users.noreply.github.com>
2022-12-08 14:59:42 +0800

    Added Boss to Level 2

---
+++ Assets/Scripts/Boss 2/BossBehavior2.cs
@@ -0,0 +1,116 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Animations;
+public class BossBehavior2 : MonoBehaviour
+{
+    [SerializeField] string playerTag = "Player";
+    [SerializeField] string WorldSpaceCanvas = "WorldSpace";
+   // [SerializeField] GameObject UITimer;
+    [SerializeField] int damageAmount = 2;
+    //[SerializeField] Animator animator;
+    [SerializeField] private GameObject[] shieldPrefab;
+    private Image timerBar;
+    Transform player;
+    HPManager hpManager;
+
+    [SerializeField] float attackInterval = 4f;
+    public float attackTime;
+
+    [SerializeField] float teleportInterval = 0.5f;
+    public float tpTime;
+
+    [SerializeField] private GameObject[] NodePath;
+
+    [SerializeField] private int shieldNumber = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        //timerBar = Instantiate(UITimer, GameObject.FindGameObjectWithTag(WorldSpaceCanvas).transform).GetComponent<Image>();
+        hpManager = FindObjectOfType<HPManager>();
+        player = GameObject.FindGameObjectWithTag(playerTag).GetComponent<Transform>();
+        attackTime = attackInterval;
+      //  timerBar.gameObject.SetActive(true);
+        tpTime = teleportInterval;
+    }
+
+    private void OnEnable()
+    {
+        attackTime = attackInterval;
+        tpTime = teleportInterval;
+        //timerBar.gameObject.SetActive(true);
+    }
+
+    private void OnDisable()
+    {
+        //if (timerBar.gameObject != null)
+        //{
+        //    timerBar.gameObject.SetActive(false);
+        //}
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateBarPosition();
+        UpdateTimer();
+       LookAtPlayer();
+    }
+
+    void LookAtPlayer()
+    {
+        transform.right = player.transform.position - transform.position;
+    }
+
+    void UpdateBarPosition()
+    {
+        //timerBar.transform.position = transform.position + new Vector3(0, 1, 0);
+        //timerBar.transform.forward = player.transform.position - timerBar.transform.position;
+    }
+
+    void UpdateTimer()
+    {
+        if (attackTime <= 0f)
+        {
+            Attack();
+            attackTime = attackInterval;
+        }
+        if (tpTime <= 0f){
+            UpdatePosition();
+            tpTime = teleportInterval;
+        }
+        else
+        {
+            attackTime -= Time.deltaTime;
+            tpTime -= Time.deltaTime;
+        }
+       // timerBar.fillAmount = attackTime / attackInterval;
+    }
+
+    void Attack()
+    {
+        //animator.SetTrigger("Attack");
+        hpManager.dealDmg(damageAmount);
+    }
+
+    void UpdatePosition()
+    {
+        int set = Random.Range(0, NodePath.Length + 1);
+        this.transform.SetPositionAndRotation(NodePath[set].transform.position, Quaternion.identity);
+        if (shieldNumber < 8)
+        {
+            generateShield();
+            shieldNumber++;
+        }
+    }
+
+    void generateShield()
+    {
+        int type = Random.Range(0, 2);
+        GameObject shield = Instantiate(shieldPrefab[type], new Vector3(this.transform.position.x + (shieldNumber * 0.5f), this.transform.position.y, this.transform.position.z), Quaternion.identity);
+        shield.transform.parent = this.transform;
+    }
+}
