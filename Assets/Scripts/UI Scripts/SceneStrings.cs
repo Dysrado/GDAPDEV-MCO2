f29035eea648a1bc1a7b1fd3181fbe51da07bcd4
Dysrado <63017345+Dysrado@users.noreply.github.com>
2022-12-09 20:14:20 +0800

    aDDED THE TRUE SECOND LEVEL

--- Assets/Scripts/UI Scripts/SceneStrings.cs
+++ Assets/Scripts/UI Scripts/SceneStrings.cs
@@ -6,6 +6,6 @@ public class SceneStrings
 {
     public const string MAIN_MSCENE = "MainMenu";
     public const string LEVEL_ONE_SCENE = "SampleScene";
-    public const string LEVEL_TWO_SCENE = "2nd Level (R)";
+    public const string LEVEL_TWO_SCENE = "Level2";
     public const string LEVEL_THREE_SCENE = "Level3";
 }
