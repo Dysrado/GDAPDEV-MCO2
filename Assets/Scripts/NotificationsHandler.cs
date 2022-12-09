e8acb7f7614e5d1bb24708a4791456f1fad9d33f
Wayne Michael Pangilinan <113033667+Makiiidon@users.noreply.github.com>
2022-12-09 19:11:39 +0800

    Added the Debug Menu and Pause

--- Assets/Scripts/NotificationsHandler.cs
+++ Assets/Scripts/NotificationsHandler.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using Unity.Notifications.Android;
 
 public class NotificationsHandler : MonoBehaviour
@@ -8,6 +9,10 @@ public class NotificationsHandler : MonoBehaviour
     [SerializeField] string notif_title = "Hey you!";
 
     [SerializeField] string notif_message = ":>";
+
+    [SerializeField] int seconds = 5;
+
+    [SerializeField] TMP_Text timeText;
 
     private void Awake()
     {
@@ -33,8 +38,23 @@ public class NotificationsHandler : MonoBehaviour
     {
         System.DateTime fireTime = System.DateTime.Now.AddSeconds(10);
 
+        System.TimeSpan interval = new System.TimeSpan(0, 0, seconds);
         AndroidNotification notif = new AndroidNotification(notif_title, notif_message, fireTime);
 
         AndroidNotificationCenter.SendNotification(notif, "default");
     }
+
+    public void AddTime()
+    {
+        seconds += 1;
+        timeText.text = seconds.ToString();
+    }
+
+    public void MinusTime()
+    {
+        seconds -= 1;
+        timeText.text = seconds.ToString();
+    }
+
+    
 }
